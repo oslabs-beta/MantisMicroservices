4962b22957bf9368c5ccf9fd5529606cf6c16597
DonnaIsabel97 <tpr.th.bvb@gmail.com>
2025-02-22 17:12:31 -0500

    vite erase extra plugin

--- mantis/vite.config.ts
+++ mantis/vite.config.ts
@@ -8,7 +8,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  plugins: [react(), tailwindcss()],
   server: {
     // change the port to the one we are going to use for backend, same with endpoint
     proxy: {
